efe1e203cae930ead011502346f425f58b469cd5
Oleksii Kozhemiaka <olesis.ko@google.com>
2023-03-14 02:11:36 +0300

    added forgotten files

---
+++ src/IdentityServer/Migrations/20230313230003_VerificationSmsCodeAdded.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class VerificationSmsCodeAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VerifySmsCodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Code = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerifySmsCodes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VerifySmsCodes");
+        }
+    }
+}
